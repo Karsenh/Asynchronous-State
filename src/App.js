37723a0dc80a0e73377fd537013beb851c3a6460
Karsen H <karsenhansen@gmail.com>
2021-05-15 12:13:53 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      number: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        return { number: this.state.number + 1 };
+      },
+      () => console.log(this.state.number)
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter</h1>
+        <p>{this.state.number}</p>
+        <button onClick={this.handleClick}> Update Count </button>
+      </div>
+    );
+  }
+}
+
+export default App;
